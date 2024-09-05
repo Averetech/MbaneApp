1f899496405662fdfd941f307f85aa1f72f64ebe
Zenande Mbane <87269989+Averetech@users.noreply.github.com>
2024-09-05 20:39:31 +0200

    updated HTML and added some JS

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ function refreshWeatherData(response) {
   let searchedCity = document.querySelector("#searched-city");
   let currentTemp = document.querySelector("#current-temp");
   let temperature = response.data.temperature.current;
+  let tempDescription = document.querySelector("#temp-description");
+  let windSpeed = document.querySelector("#wind-speed");
+  let speed = response.data.wind.speed;
+  let theHumidity = document.querySelector("#humidity");
+  let humidit = response.data.temperature.humidity;
+
+  tempDescription.innerHTML = response.data.condition.description;
   searchedCity.innerHTML = response.data.city;
   currentTemp.innerHTML = Math.round(temperature);
+  windSpeed.innerHTML = Math.round(speed);
+  theHumidity.innerHTML = humidit;
 }
 
 function getCityObect(city) {
